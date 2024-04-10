bb023c4e826be2fdd9f0df84ab8b1b56b296fce2
Amirai1129 <150271657+Amirai1129@users.noreply.github.com>
2024-04-10 12:15:19 +0200

    Update info.py

--- info.py
+++ info.py
@@ -124,7 +124,7 @@ if len(BIN_CHANNEL) == 0:
     exit()
 else:
     BIN_CHANNEL = int(BIN_CHANNEL)
-URL = environ.get("URL", "http://serverdlfile.online")
+URL = environ.get("URL", "")
 if len(URL) == 0:
     print('Error - URL is missing, exiting now')
     exit()
